7491eb331c900899550484c25d746e972c5e62ec
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-21 02:11:10 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,16 +1,9 @@
 class Cat
 
   attr_accessor :name
-  attr_accessor :meow
+  def meow
+    puts "meow!"
+  end
 
 
 end
-
-maru = Cat.new
-maru.name = "Maru"
-
-puts maru.name
-
-maru.meow = "meow!"
-
-puts maru.meow
